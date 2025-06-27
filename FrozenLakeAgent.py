8fb5a0e6aa7677572cf3f5bb2d6ebd0c322ce6f8
idobenzvibrenner <ido.benzvibrenner@mail.huji.ac.il>
2025-06-27 13:07:53 +0300

    vcbv

---
+++ FrozenLakeAgent.py
@@ -0,0 +1,80 @@
+from collections import defaultdict
+import gymnasium as gym
+import numpy as np
+
+
+class FrozenLakeAgent:
+    def __init__(
+        self,
+        env: gym.Env,
+        learning_rate: float,
+        initial_epsilon: float,
+        epsilon_decay: float,
+        final_epsilon: float,
+        discount_factor: float = 0.95,
+    ):
+        """Initialize a Reinforcement Learning agent with an empty dictionary
+        of state-action values (q_values), a learning rate and an epsilon.
+
+        Args:
+            env: The training environment
+            learning_rate: The learning rate
+            initial_epsilon: The initial epsilon value
+            epsilon_decay: The decay for epsilon
+            final_epsilon: The final epsilon value
+            discount_factor: The discount factor for computing the Q-value
+        """
+        self.env = env
+        self.q_values = defaultdict(lambda: np.zeros(env.action_space.n))
+
+        self.lr = learning_rate
+        self.discount_factor = discount_factor
+
+        self.epsilon = initial_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.final_epsilon = final_epsilon
+
+        self.training_error = []
+
+    def get_action(self, obs: int) -> int:
+        """
+        Returns the best action with probability (1 - epsilon)
+        otherwise a random action with probability epsilon to ensure exploration.
+        """
+        # with probability epsilon return a random action to explore the environment
+        if np.random.random() < self.epsilon:
+            return self.env.action_space.sample()
+        # with probability (1 - epsilon) act greedily (exploit)
+        else:
+            return int(np.argmax(self.q_values[obs]))
+
+    def update(
+        self,
+        obs: int,
+        action: int,
+        reward: float,
+        terminated: bool,
+        next_obs: int,
+    ):
+        """Updates the Q-value of an action."""
+        future_q_value = (not terminated) * np.max(self.q_values[next_obs])
+        temporal_difference = (
+            reward + self.discount_factor * future_q_value - self.q_values[obs][action]
+        )
+
+        self.q_values[obs][action] = self.q_values[obs][action] + self.lr * temporal_difference
+        self.training_error.append(temporal_difference)
+
+    def decay_epsilon(self):
+        self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
+
+    def print(self):
+        """
+        Print the Q-values in a sorted and readable format.
+        Each line shows the state and its corresponding action values.
+        """
+        print("Q-values:")
+        for state in sorted(self.q_values.keys()):
+            actions = self.q_values[state]
+            action_values = ", ".join(f"{a}: {v:.2f}" for a, v in enumerate(actions))
+            print(f"State {state}: {action_values}")
---
+++ main.py
@@ -0,0 +1,36 @@
+import gymnasium as gym
+from FrozenLakeAgent import FrozenLakeAgent
+from BlackJackAgent import BlackjackAgent
+
+# env = gym.make('Blackjack-v1', natural=False, sab=False, render_mode="human")
+env = gym.make('ALE/Pacman-v5')
+
+lr = 0.1
+epsilon = 0.1
+epsilon_decay = 0.002
+final_epsilon = 0
+discount_factor = 1
+agent = BlackjackAgent(env, lr, epsilon, epsilon_decay, final_epsilon, discount_factor)
+
+# Reset the environment to generate the first observation
+observation, info = env.reset(seed=42)
+for _ in range(1000):
+    # this is where you would insert your policy
+    action = agent.get_action(observation)
+    # action = env.action_space.sample()
+
+    # step (transition) through the environment with the action
+    # receiving the next observation, reward and if the episode has terminated or truncated
+    next_observation, reward, terminated, truncated, info = env.step(action)
+
+    # Update agent
+    agent.update(observation, action, float(reward), terminated, next_observation)
+    observation = next_observation
+    agent.decay_epsilon()
+
+    # If the episode has ended then we can reset to start a new episode
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
+agent.print()
