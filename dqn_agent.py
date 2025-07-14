4a852b85e0468b08299f59114f31503f831904c9
gil.harari2 <gil.harari2@mail.huji.ac.il>
2025-07-14 22:54:22 +0300

    added loading of saved models, and it runs now

--- dqn_agent.py
+++ dqn_agent.py
@@ -14,10 +14,10 @@ class DQNAgent:
         self.model.eval()
 
     def select_action(self, state):
-        state = torch.FloatTensor(state).unsqueeze(0)
         with torch.no_grad():
-            q_values = self.model(state)
-        return int(torch.argmax(q_values, dim=1).item())
+            temp_state = state_to_dqn_input(state)
+            action = self.model(temp_state).argmax().item()
+            return action
 
     def play(self, gameName , episodes=3):
         gym.register_envs(ale_py)
@@ -26,20 +26,21 @@ class DQNAgent:
         env = FrameStackObservation(env, stack_size=4)
 
         for ep in range(episodes):
-            state = env.reset()
-            done = False
+            state = env.reset()[0]
+            terminated, truncated = False, False
             total_reward = 0
-            while not done:
-                state_tensor = state_to_dqn_input(state)
-                action = self.select_action(state_tensor)
-                state, reward, done, _ = env.step(action)
+            while (not terminated and not truncated):
+                action = self.select_action(state)
+                state, reward, terminated, truncated, _  = env.step(action)
                 total_reward += reward
             print(f"Episode {ep+1}: Total Reward = {total_reward}")
 
         env.close()
 
-# Example usage:
-# import gym
-# env = gym.make('CartPole-v1')
-# agent = DQNAgent(state_dim=4, action_dim=2, weights_path='dqn_weights.pt')
-# agent.play(env, episodes=5, render=True)
+if __name__ == "__main__":
+    gameName = 'MsPacmanNoFrameskip-v4'
+    weights_path = f'output/weights/{gameName}.pt'  # Adjust the path as needed
+    num_actions = 9  # Example number of valid actions
+
+    agent = DQNAgent(num_actions, weights_path)
+    agent.play(gameName, episodes=10)  # Play with the trained agent
