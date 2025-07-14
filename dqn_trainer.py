ac32521e1b0fb5bc737af143023f28c0d13072a5
gil.harari2 <gil.harari2@mail.huji.ac.il>
2025-07-14 23:55:57 +0300

    removed CUDA optimization folder. moved CUDA optimized trainer to the regular file

--- dqn_trainer.py
+++ dqn_trainer.py
@@ -11,154 +11,178 @@ import random
 
 from ReplayMemory import ReplayMemory
 from DQN import DQN, state_to_dqn_input
+import time
 
 gym.register_envs(ale_py)
-output_dir = 'output'  # Directory to save the model and plots
+output_dir = 'output'
 weights_path = f"{output_dir}/weights"
 plots_path = f"{output_dir}/plots"
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
 class DQNtrainer():
-    # Hyperparameters (adjustable)
-    learning_rate_a = 0.00025       # learning rate (alpha)
-    discount_factor_g = 0.99        # discount rate (gamma)    
-    network_sync_rate = 100         # number of steps the agent takes before syncing the policy and target network
-    replay_memory_size = 10000      # size of replay memory
-    mini_batch_size = 32            # size of the training data set sampled from the replay memory
+    learning_rate_a = 0.00025
+    discount_factor_g = 0.99
+    network_sync_rate = 100
+    replay_memory_size = 10000
+    mini_batch_size = 32
+    train_frequency = 4  # Frequency of training updates
+    save_model_frequency = 100 # Save model every 100 episodes
 
-    # Neural Network
-    loss_fn = nn.MSELoss()          # NN Loss function. MSE=Mean Squared Error can be swapped to something else.
-    optimizer = None                # NN Optimizer. Initialize later.
+    model = None
+    loss_fn = nn.MSELoss()
+    loss_list = []
+    optimizer = None
 
-    # Actions for the Atari environment
-    num_actions = None  # This will be set during training
-    
-    def train(self, episodes, gameName='BreakoutNoFrameskip-v4', render=False):
-        # environment setup
+    num_actions = None
+    total_steps = 0
+
+    def train(self, episodes, gameName='BreakoutNoFrameskip-v4', render=False, disable_frame_skips=False):
+        # Environment setup
         env = gym.make(gameName, render_mode='human' if render else None)
-        env = AtariPreprocessing(env, grayscale_obs=True)
+        env = AtariPreprocessing(env, grayscale_obs=True, frame_skip=1 if disable_frame_skips else 4)
+        env = gym.wrappers.RecordEpisodeStatistics(env)
         env = FrameStackObservation(env, stack_size=4)
         self.num_actions = env.action_space.n
         state, _ = env.reset()
-        state, reward, done, truncated, info = env.step(env.action_space.sample())
+        gameName = gameName.replace('/', '_')  # Sanitize game name for file paths
 
-        # misc setup
-        epsilon = 1 # 1 = 100% random actions
+        epsilon = 1
         memory = ReplayMemory(self.replay_memory_size)
         rewards_per_episode = np.zeros(episodes)
         epsilon_history = []
 
-        # network setup
-        policy_dqn = DQN(self.num_actions)
-        target_dqn = DQN(self.num_actions)
+        policy_dqn = DQN(self.num_actions).to(device) if self.model is None else self.model
+        target_dqn = DQN(self.num_actions).to(device)
         target_dqn.load_state_dict(policy_dqn.state_dict())
-        step_count=0
+        step_count = 0
         self.optimizer = torch.optim.Adam(policy_dqn.parameters(), lr=self.learning_rate_a)
 
         for i in range(episodes):
             print(f"Episode {i+1}/{episodes} started.")
-            
-            # init environment
             state = env.reset()[0]
             terminated, truncated = False, False
             rewards_per_episode[i] = 0
 
-            while(not terminated and not truncated):
-                # Select action based on epsilon-greedy
+            # print(f"Initial state shape: {state.shape}")
+            # for i in range(4):  # Ensure we have 4 stacked frames
+            #     state, _, terminated, truncated, _ = env.step(env.action_space.sample())
+            #     image = Image.fromarray(state[i])
+            #     image.show(title=f"Initial State - Episode {i+1}")
+            # return
+
+            while not terminated and not truncated:
                 if random.random() < epsilon:
                     action = env.action_space.sample()
                 else:
                     with torch.no_grad():
-                        temp_state = state_to_dqn_input(state)
+                        temp_state = state_to_dqn_input(state).to(device)
                         action = policy_dqn(temp_state).argmax().item()
 
-                # Take action in the environment
-                new_state,reward,terminated,truncated,_ = env.step(action)
-                memory.append((state, action, new_state, reward, terminated)) 
+                new_state, reward, terminated, truncated, _ = env.step(action)
+                memory.append((state, action, new_state, reward, terminated))
                 state = new_state
                 rewards_per_episode[i] += reward
-                step_count+=1
+                step_count += 1
+                self.total_steps += 1
 
-                # Check if enough experience has been collected
-                if len(memory)>self.mini_batch_size and np.sum(rewards_per_episode[:i+1]) > 0:
+                if len(memory) > self.mini_batch_size and self.total_steps % self.train_frequency == 0:
                     mini_batch = memory.sample(self.mini_batch_size)
                     self.optimize(mini_batch, policy_dqn, target_dqn)
 
-                    # Copy policy network to target network after a certain number of steps
                     if step_count > self.network_sync_rate:
                         target_dqn.load_state_dict(policy_dqn.state_dict())
-                        step_count=0
+                        step_count = 0
 
-            # Decay epsilon
-            epsilon = max(epsilon - 1/episodes, 0)
+            epsilon = max(epsilon - 1 / episodes, 0)
             epsilon_history.append(epsilon)
 
-        # Close environment
-        env.close()
+            if i % self.save_model_frequency == 0:
+                self.save_model(policy_dqn, f"{weights_path}/{gameName}_episode_{i}.pt")
 
-        # Save policy
+            # print total steps so far
+            print(f"Total steps so far: {self.total_steps}")
+            
+        env.close()
         fileName = f"{weights_path}/{gameName}.pt"
         torch.save(policy_dqn.state_dict(), fileName)
 
-        # Create new graph 
-        plt.figure(1)
-
-        # Plot average rewards (Y-axis) vs episodes (X-axis)
-        sum_rewards = np.zeros(episodes)
-        for x in range(episodes):
-            sum_rewards[x] = np.sum(rewards_per_episode[max(0, x-100):(x+1)])
-        plt.subplot(121) # plot on a 1 row x 2 col grid, at cell 1
-        plt.plot(sum_rewards)
-        
-        # Plot epsilon decay (Y-axis) vs episodes (X-axis)
-        plt.subplot(122) # plot on a 1 row x 2 col grid, at cell 2
-        plt.plot(epsilon_history)
-        
-        # Save plots
-        plotName = f"{plots_path}/{gameName}_plot.png"
-        plt.savefig(plotName)
+        self.plot_results(rewards_per_episode, epsilon_history, gameName)
 
         return fileName
 
     def optimize(self, mini_batch, policy_dqn, target_dqn):
-
-        poilcy_q_values = []
+        policy_q_values = []
         target_q_values = []
 
         for state, action, new_state, reward, terminated in mini_batch:
+            state_tensor = state_to_dqn_input(state).to(device)
+            new_state_tensor = state_to_dqn_input(new_state).to(device)
 
-            if terminated: 
-                # Agent either reached goal (reward=1) or fell into hole (reward=0)
-                # When in a terminated state, target q value should be set to the reward.
-                y = torch.FloatTensor([reward])
+            if terminated:
+                y = torch.tensor([reward], dtype=torch.float32, device=device)
             else:
-                # Calculate target q value 
                 with torch.no_grad():
-                    new_state = state_to_dqn_input(new_state)
-                    target_max_value = target_dqn(new_state).max()  # Get the maximum Q value for the next state
+                    target_max_value = target_dqn(new_state_tensor).max()
+                    y = torch.tensor([reward + self.discount_factor_g * target_max_value], device=device)
 
-                    y = torch.FloatTensor(
-                        reward + self.discount_factor_g * target_max_value
-                    )
-
-            # Get the current set of Q values
-            current_q_value = policy_dqn(state_to_dqn_input(state))
-            poilcy_q_values.append(current_q_value)
-
-            # Get the target set of Q values
-            target_q = target_dqn(state_to_dqn_input(state))
-            
-            # Adjust the specific action to the target that was just calculated
+            current_q = policy_dqn(state_tensor)
+            target_q = current_q.clone().detach()
             target_q[0][action] = y
-            target_q_values.append(target_q)
-                
-        # Compute loss for the whole minibatch
-        loss = self.loss_fn(torch.stack(poilcy_q_values), torch.stack(target_q_values))
 
-        # Optimize the model
+            policy_q_values.append(current_q)
+            target_q_values.append(target_q)
+
+        loss = self.loss_fn(torch.stack(policy_q_values), torch.stack(target_q_values))
+        self.loss_list.append(loss.item()) 
+
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
+    def save_model(self, model, file_name):
+        torch.save(model.state_dict(), file_name)
+        print(f"Model saved to {file_name}")
+
+    def load_model(self, file_name):
+        self.model.load_state_dict(torch.load(file_name, map_location=device))
+        self.model.eval()
+
+    def plot_results(self, rewards_per_episode, epsilon_history, gameName):
+        plt.figure(1)
+
+        # Plot raw reward per episode
+        plt.subplot(121)
+        plt.plot(rewards_per_episode)
+        plt.title("Reward per Episode")
+        plt.xlabel("Episode")
+        plt.ylabel("Reward")
+
+        # Plot epsilon decay
+        plt.subplot(122)
+        plt.plot(epsilon_history)
+        plt.title("Epsilon Decay")
+        plt.xlabel("Episode")
+        plt.ylabel("Epsilon")
+
+        # Save the figure
+        plotName = f"{plots_path}/{gameName}_plot.png"
+        plt.savefig(plotName)
+
+        # plot enumarted loss_list
+        plt.figure(2)
+        plt.plot(self.loss_list)
+        plt.title("Loss per Optimization Step")
+        plt.xlabel("Optimization Step")
+        plt.ylabel("Loss")
+        plt.savefig(f"{plots_path}/{gameName}_loss_plot.png")
+
 if __name__ == "__main__":
+    start_time = time.time()
     pacman = DQNtrainer()
-    pacman.train(episodes=10, render=False)  # Adjust the number of episodes as needed
+    pacman.train(episodes=100, gameName='ALE/Breakout-v5', render=False, disable_frame_skips=True)
+    # pacman.train(episodes=500, gameName='MsPacmanNoFrameskip-v4', render=False)
+    # pacman.train(episodes=500, render=False)
+    end_time = time.time()
+    print(f"Training completed in {end_time - start_time:.2f} seconds.")
